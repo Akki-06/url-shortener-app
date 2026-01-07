8e52797a96247e02891778f3ab8aa9dc40a40f13
Akhil <180540953+Akki-06@users.noreply.github.com>
2026-01-07 14:59:55 +0530

    Fixing env variables

--- client/src/components/Shortener.jsx
+++ client/src/components/Shortener.jsx
@@ -6,7 +6,7 @@ function Shortener() {
     const [shortUrl, setShortUrl] = useState("");
     const [error, setError] = useState("");
     const [copied, setCopied] = useState(false);
-    const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 
     const handleShorten = async () => {
